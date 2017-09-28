aef97085aab31b22f237a3fe9938bcc9009b11ea
Nicholas Giordano <nicholas.giordano@live.com>
2017-09-28 20:09:38 +0200

    Mia notissima frase

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ bot.on(['/spacco'], (msg) => msg.reply.text('SHHPACCO TUTTOOO!'));
 
 bot.on(['/linux'], (msg) => msg.reply.text('MMM Linux come sei gustoso!'));
 
+bot.on(['/lacava'], (msg) => msg.reply.text('MMM Meglio ucciderlo!'));
+
 bot.on('photo', (msg) => msg.reply.text('Mi spiace mi interessano solo foto di tua madre'));
 
 //bot.on('text', (msg) => msg.reply.text(msg.text));
