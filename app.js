310b42d1d10ebdcb64155d060ea008cbb83c553f
Andrea Lacava <thecave003@gmail.com>
2017-09-29 20:19:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ bot.on(['/spacco'], (msg) => msg.reply.text('SHHPACCO TUTTOOO!'));
 
 bot.on(['/linux'], (msg) => msg.reply.text('MMM Linux come sei gustoso!'));
 
-bot.on(['/lacava'], (msg) => msg.reply.text('MMM Meglio ucciderlo!'));
+bot.on(['/lacava'], (msg) => msg.reply.text('MMM Il mio padrone supremo!'));
 
 bot.on('photo', (msg) => msg.reply.text('Mi spiace mi interessano solo foto di tua madre'));
 
